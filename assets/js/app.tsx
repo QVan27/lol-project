a93982f346e3a05e1ab9c6a7db6e62244e224eba
QVan27 <qvannarath@gmail.com>
2022-09-27 09:48:45 +0200

    first commit

---
+++ assets/js/app.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import '../scss/app.scss';
+import { ROOT } from "./constants/general";
+// import Header from "./components/shared/Header";
+import { ABOUT, HOME, POST_DETAIL, POSTS } from "./constants/route-paths";
+
+import HomePage from "./components/pages/HomePage";
+// import AboutPage from "./components/pages/AboutPage";
+// import PostDetailPage from "./components/pages/PostDetailPage";
+// import PostsPage from "./components/pages/PostsPage";
+
+const App: FunctionComponent = () => {
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Switch>
+        <Route exact={ true } path={ HOME } component={ HomePage } />
+        {/* <Route exact={ true } path={ ABOUT } component={ AboutPage } /> */}
+        {/* <Route exact={ true } path={ POST_DETAIL } component={ PostDetailPage } /> */}
+        {/* <Route exact={ true } path={ POSTS } component={ PostsPage } /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById(ROOT));
+
---
+++ assets/js/components/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react';
+// import { Col, Container, Form, FormGroup, Input, Label } from "reactstrap";
+// import { useInput } from "../hooks/useInput";
+// import axios from '../../config/axios';
+
+const HomePage: FunctionComponent = () => {
+//   const { value: title, bind, reset } = useInput('');
+
+//   const handleSubmit = (evt: any) => {
+//     evt.preventDefault();
+//     axios.post('/post/create', { title }).then((res) => {
+//       console.log(res.data);
+//     });
+//     reset();
+//   };
+
+  return (
+    <section></section>
+  );
+};
+
+export default HomePage;
