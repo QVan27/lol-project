fdb1c031b901f24c8a05628e415e99947c32d8e7
QVan27 <qvannarath@gmail.com>
2022-09-28 10:16:23 +0200

    💄 Add Styled-Components and Update Header / Footer SCSS

--- assets/js/components/pages/HomePage.tsx
+++ assets/js/components/pages/HomePage.tsx
@@ -1,22 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 // import { Col, Container, Form, FormGroup, Input, Label } from "reactstrap";
 // import { useInput } from "../hooks/useInput";
 // import axios from '../../config/axios';
 
 const HomePage: FunctionComponent = () => {
-//   const { value: title, bind, reset } = useInput('');
+  //   const { value: title, bind, reset } = useInput('');
 
-//   const handleSubmit = (evt: any) => {
-//     evt.preventDefault();
-//     axios.post('/post/create', { title }).then((res) => {
-//       console.log(res.data);
-//     });
-//     reset();
-//   };
+  //   const handleSubmit = (evt: any) => {
+  //     evt.preventDefault();
+  //     axios.post('/post/create', { title }).then((res) => {
+  //       console.log(res.data);
+  //     });
+  //     reset();
+  //   };
 
   return (
     <main>
-      <section></section>
+      <section className="home">
+        <div className="wrap-main">
+          <div className="home__container"></div>
+        </div>
+      </section>
     </main>
   );
 };
