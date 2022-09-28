4a128d90dcc81bee974a522bdc8b1d38f609b6f6
Guillaume Merat <guillaume.merat@campus-la-chataigneraie.org>
2022-09-28 11:50:48 +0200

    fonction showjson

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\CallApiService;
+use App\Controller\ManagerRegistry;
+use App\Repository\ClassesRepository;
+use Doctrine\ORM\EntityManger;
+use App\Repository\APIJSONRepository;
+use App\Entity\APIJSON;
 
 
 class ApiController extends AbstractController
 {
+
+
     #[Route('/api', name: 'app_api')]
     public function index(CallApiService $callApiService): Response
-    { 
+    {
+        
         dd($callApiService->getData());
         return $this->render('api/index.html.twig', [
             'controller_name' => 'ApiController',
@@ -20,8 +28,22 @@ class ApiController extends AbstractController
             
             
         ]);
-
    
     }
+
+    #[Route('/script_json', name: 'script_json')]
+    public function showJson() : Response
+    {
+        $json = $this->getDoctrine()
+            ->getRepository(APIJSON::class)->findAll();
+        
+        // return $this->render('api/index.html.twig', [
+        //     'json' =>$json
+        // ]);
+        dd($json);
+
+    }
+
+    
 
 }
